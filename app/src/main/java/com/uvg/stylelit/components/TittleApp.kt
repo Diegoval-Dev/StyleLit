a3a415d0ec000f809cbc20b4dc0830022028cee9
Anaru03 <dele22428@uvg.edu.gt>
2023-09-09 14:15:49 -0600

    Finalización de titulo
    A través de "modifier" se crearon 3 capas para crear un titulo creativo y que tenga una buena previsualización.

--- app/src/main/java/com/uvg/stylelit/components/TittleApp.kt
+++ app/src/main/java/com/uvg/stylelit/components/TittleApp.kt
@@ -1,45 +1,67 @@
 package com.uvg.stylelit.components
-//Referencia de: https://developer.android.com/jetpack/compose/text?hl=es-419
 
+import android.os.Bundle
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun TitleWithShadow(text: String, shadowColor: Color) {
+fun TituloPrincipal() {
     Text(
-        text = text,
+        text = "MEN'S",
         style = TextStyle(
-            fontSize = 24.sp,
-            shadow = Shadow(
-                color = shadowColor,
-                offset = Offset(2.0f, 4.0f),
-                blurRadius = 3f
-            )
-        )
+            fontSize = 40.sp,
+            color = Color(0XFFFFFFFF),
+            fontFamily = FontFamily.SansSerif
+        ),
+        modifier = Modifier
+            .width(187.dp)
+            .height(45.dp)
+            .offset(13.dp, 1.dp)
+    )
+
+    Text(
+        text = "MEN'S",
+        style = TextStyle(
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 40.sp,
+            color = Color(0XFF0C95C0)
+        ),
+        modifier = Modifier
+            .width(187.dp)
+            .height(45.dp)
+            .offset(12.dp, 1.dp)
+    )
+
+    Text(
+        text = "MEN'S",
+        style = TextStyle(
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 40.sp,
+            color = Color(0XFFFFFFFF),
+        ),
+        modifier = Modifier
+            .width(187.dp)
+            .height(45.dp)
+            .offset(10.dp, 1.dp)
     )
 }
 
-@Composable
-fun TitleWithShadowDemo() {
-    TitleWithShadow(
-        text = "Mi Título",
-        shadowColor = Color(0XFF0C95C0)
-    )
-}
 
 @Preview
 @Composable
-fun TitleWithShadowPreview() {
-    TitleWithShadow(
-        text = "Mi Título",
-        shadowColor = Color(0XFF0C95C0)
-    )
+fun TitloPrinPreview() {
+    MaterialTheme {
+        TituloPrincipal()
+    }
 }
-
