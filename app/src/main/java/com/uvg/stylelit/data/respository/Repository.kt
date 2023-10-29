0929a192b9998d06ac7f08f1adbc7f33b2db73e5
Diego <diegopablo012@gmail.com>
2023-10-29 14:04:09 -0600

    Add Firebase dependencies for connectivity

--- app/src/main/java/com/uvg/stylelit/data/respository/Repository.kt
+++ app/src/main/java/com/uvg/stylelit/data/respository/Repository.kt
@@ -1,2 +1,8 @@
 package com.uvg.stylelit.data.respository
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class Repository {
+    val db = Firebase.firestore
+}
