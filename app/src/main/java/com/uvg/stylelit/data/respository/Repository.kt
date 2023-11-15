ef778a94d0e99b94e63e83f8faf592a3f9481c17
Anaru03 <dele22428@uvg.edu.gt>
2023-11-14 20:03:14 -0600

    Arreglos en la arquitectura

--- app/src/main/java/com/uvg/stylelit/data/respository/Repository.kt
+++ app/src/main/java/com/uvg/stylelit/data/respository/Repository.kt
@@ -1,8 +1,21 @@
 package com.uvg.stylelit.data.respository
 
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class Repository {
     val db = Firebase.firestore
+    val blusas = mutableListOf<DocumentReference>()
+
+    init {
+        for (i in 1..5) {
+            val blusaRef = db.collection("Woman")
+                .document("Camisas")
+                .collection("B. Larga")
+                .document("blusa$i")
+            blusas.add(blusaRef)
+        }
+    }
+
 }
